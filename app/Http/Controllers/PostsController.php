fd1557a55b4277080dacae63bd03c1d35183da13
Tony-S201 <swierz.tony@gmail.com>
2020-07-15 14:23:33 +0200

    Use model for a Post instead of the previous method

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,14 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use DB;
+use App\Post;
+
 class PostsController extends Controller
 {
     public function show($slug) {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
-
-        if($post == null) {
-            abort('404', 'The post does not exist');
-        }
+        $post = Post::where('slug', $slug)->firstOrFail();
 
         return view('post', [
             'post' => $post
