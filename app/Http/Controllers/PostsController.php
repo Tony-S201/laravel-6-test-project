f72c451dce6435355c59a4669f4637495f3b285e
Tony-S201 <swierz.tony@gmail.com>
2020-07-15 13:41:25 +0200

    Return different content from database depending on url

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,7 +6,16 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    public function show($post) {
-        return view('welcome');
+    public function show($slug) {
+        $post = \DB::table('posts')->where('slug', $slug)->first();
+
+        if($post == null) {
+            abort('404', 'The post does not exist');
+        }
+
+        return view('post', [
+            'post_title' => $post->slug,
+            'post_text' => $post->body
+        ]);
     }
 }
